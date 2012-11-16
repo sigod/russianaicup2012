162f223c12e9e4c8faad604c36a23b7658699186
Michael Novikov <sigod.mail@gmail.com>
2012-11-16 17:19:11 +0300

    Implement simple strategy;

--- sources/MyStrategy.cs
+++ sources/MyStrategy.cs
@@ -5,12 +5,79 @@ namespace Com.CodeGame.CodeTanks2012.DevKit.CSharpCgdk
 {
 	public sealed class MyStrategy : IStrategy
 	{
+		private readonly double MIN_ANGLE = Math.PI / 180.0;
+
 		public void Move(Tank self, World world, Move move)
 		{
-			move.LeftTrackPower = -1.0D;
-			move.RightTrackPower = 1.0D;
-			move.TurretTurn = Math.PI;
-			move.FireType = FireType.PremiumPreferred;
+			double angleToTank = double.MaxValue;
+			Tank selectedTank = null;
+
+			foreach (Tank tank in world.Tanks)
+			{
+				if (tank.IsTeammate) continue;
+				if (tank.CrewHealth < 1) continue;
+
+				double angle = self.GetTurretAngleTo(tank);
+
+				if (Math.Abs(angle) < Math.Abs(angleToTank))
+				{
+					angleToTank = angle;
+					selectedTank = tank;
+				}
+			}
+
+			if (selectedTank != null)
+			{
+				if (angleToTank > MIN_ANGLE)
+					move.TurretTurn = 1.0d;
+				else if (angleToTank < -MIN_ANGLE)
+					move.TurretTurn = -1.0d;
+				else
+					move.FireType = FireType.PremiumPreferred;
+			}
+
+
+			double distanceToBonus = double.MaxValue;
+			Bonus selectedBonus = null;
+
+			foreach (Bonus bonus in world.Bonuses)
+			{
+				double distance = self.GetDistanceTo(bonus);
+
+				if (distance < distanceToBonus)
+				{
+					distanceToBonus = distance;
+					selectedBonus = bonus;
+				}
+			}
+
+			if (selectedBonus != null)
+			{
+				double angle = self.GetAngleTo(selectedBonus);
+
+				if (angle > MIN_ANGLE)
+				{
+					move.LeftTrackPower = 1.0d;
+					move.RightTrackPower = 1.0d - Math.Abs(angle) * 2d;
+				}
+				else if (angle < -MIN_ANGLE)
+				{
+					move.LeftTrackPower = 1.0d - Math.Abs(angle) * 2d;
+					move.RightTrackPower = 1.0d;
+				}
+				else
+				{
+					move.LeftTrackPower = 1.0d;
+					move.RightTrackPower = 1.0d;
+				}
+
+				if (Math.Abs(angle) > Math.PI / 2)
+				{
+					double tmp = move.LeftTrackPower;
+					move.LeftTrackPower = -move.RightTrackPower;
+					move.RightTrackPower = -tmp;
+				}
+			}
 		}
 
 		public TankType SelectTank(int tankIndex, int teamSize)
