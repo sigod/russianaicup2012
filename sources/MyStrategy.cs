156560272e22f477aa9a47a4560ec2dd55f5e3b0
Michael Novikov <sigod.mail@gmail.com>
2012-11-17 12:50:50 +0300

    Separated `UnitExtensions` class;

--- sources/MyStrategy.cs
+++ sources/MyStrategy.cs
@@ -1,6 +1,4 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using Com.CodeGame.CodeTanks2012.DevKit.CSharpCgdk.Model;
 
 namespace Com.CodeGame.CodeTanks2012.DevKit.CSharpCgdk
@@ -141,29 +139,6 @@ namespace Com.CodeGame.CodeTanks2012.DevKit.CSharpCgdk
 			}
 		}
 	}
-
-	static class UnitExtensions
-	{
-		public static bool IsCovered(this Unit unit, Ray ray, World world)
-		{
-			return unit.IsCovered(ray, world.Obstacles)
-				|| unit.IsCovered(ray, world.Tanks)
-				|| unit.IsCovered(ray, world.Bonuses);
-		}
-
-		public static bool IsCovered<T>(this Unit unit, Ray ray, T by)
-			where T : ICloneable, IList, ICollection, IEnumerable
-		{
-			foreach (Unit e in by)
-			{
-				if (e.Id != ray.OriginId && e.Id != unit.Id && ray.IsCollide(e))
-					return true;
-			}
-
-			return false;
-		}
-	}
-
 
 	#region Types
 
---
+++ sources/UnitExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Com.CodeGame.CodeTanks2012.DevKit.CSharpCgdk.Model;
+
+namespace Com.CodeGame.CodeTanks2012.DevKit.CSharpCgdk
+{
+	static class UnitExtensions
+	{
+		public static bool IsCovered(this Unit unit, Ray ray, World world)
+		{
+			return unit.IsCovered(ray, world.Obstacles)
+				|| unit.IsCovered(ray, world.Tanks)
+				|| unit.IsCovered(ray, world.Bonuses);
+		}
+
+		public static bool IsCovered<T>(this Unit unit, Ray ray, T by)
+			where T : ICloneable, IList, ICollection, IEnumerable
+		{
+			foreach (Unit e in by)
+			{
+				if (e.Id != ray.OriginId && e.Id != unit.Id && ray.IsCollide(e))
+					return true;
+			}
+
+			return false;
+		}
+	}
+}
